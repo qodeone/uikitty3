1ef2ae385752a29f098000ebf3d501b30b646f1a
Steffen Rühlmann <steffen@undpaul.de>
2017-01-10 21:27:26 +0100

    update uikitty notify system - use js functions of uikit 3

--- js/notify-system.js
+++ js/notify-system.js
@@ -1,6 +1,10 @@
-        jQuery(function(){
-            jQuery("body").on("click", ".uk-button[data-message]", function(){
-                UIkit.notify(jQuery(this).data());
-            });
-            jQuery('body .uk-notify-alert').click();
-        })
+/**
+ * uikitty notification systen.
+ */
+
+jQuery(function () {
+  jQuery("body").on("click", ".uk-button[data-message]", function () {
+    UIkit.notification(jQuery(this).data());
+  });
+  jQuery('body .uk-notify-alert').click();
+})

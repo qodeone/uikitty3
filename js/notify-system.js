0201366e454909aa3c8dd786e92893cdd4ded0c9
Steffen Rühlmann <steffen@undpaul.de>
2017-01-11 11:18:13 +0100

    Fix broken UIKIt notification function

--- js/notify-system.js
+++ js/notify-system.js
@@ -1,10 +1,14 @@
 /**
  * uikitty notification systen.
  */
-
 jQuery(function () {
   jQuery("body").on("click", ".uk-button[data-message]", function () {
-    UIkit.notification(jQuery(this).data());
+    UIkit.notification({
+      message: jQuery(this).data(),
+      status: 'primary',
+      pos: 'top-right',
+      timeout: 5000
+    });
   });
   jQuery('body .uk-notify-alert').click();
 })
